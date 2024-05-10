29dc1f83a8c28f7d342e541915e1249fe8379b7d
Mario Ahumada <ahumadamob@gmail.com>
2024-05-09 22:01:10 -0300

    Creamos MascotaService sin persistir a BDD

--- src/main/java/imb/gc/p2/clases/controller/MascotaController.java
+++ src/main/java/imb/gc/p2/clases/controller/MascotaController.java
@@ -1,9 +1,13 @@
 package imb.gc.p2.clases.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import imb.gc.p2.clases.entity.Mascota;
+import imb.gc.p2.clases.service.MascotaService;
 
 @RestController
 public class MascotaController {
@@ -17,4 +21,12 @@ public class MascotaController {
 		mascota.setPeludo(true);
 		return mascota;
 	}
+	
+	@GetMapping("/mascotas")
+	public List<Mascota>infoDeTresMascotas(){
+		MascotaService service = new MascotaService();		
+		return service.getTresMascotas();
+	}
+	
+
 }
